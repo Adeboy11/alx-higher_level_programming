73fdbce4f13f48560952a05b5e70981bda3fe331
Adeboy11 <princeadeboy4real1@gmail.com>
2023-12-15 05:39:50 -0800

    rectangle changes

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """_summary_    """
 
-# from models.base import Base
-from base import Base
+from models.base import Base
+#from base import Base
 
 class Rectangle(Base):
     """_summary_
@@ -26,81 +26,81 @@ class Rectangle(Base):
         self.y = y
         super().__init__(id)
 
-        @property
-        def width(self):
-            """_summary_
-            """
-            return self.__width
+    @property
+    def width(self):
+        """_summary_
+        """
+        return self.__width
         
-        @width.setter
-        def width(self, value):
-            """_summary_
+    @width.setter
+    def width(self, value):
+        """_summary_
 
-            Args:
-                value (int): width of rectangle
-            """
-            if type(value) != int:
-                raise TypeError("width must be an int")
-            if value <= 0:
-                raise ValueError("width must be > 0")
+        Args:
+            value (int): width of rectangle
+        """
+        if type(value) != int:
+            raise TypeError("width must be an int")
+        if value <= 0:
+            raise ValueError("width must be > 0")
             self.__width = value
             
-        @property
-        def height(self):
-            """_summary_
-            """
-            return self.__height
+    @property
+    def height(self):
+        """_summary_
+        """
+        return self.__height
         
-        @height.setter
-        def height(self, value):
-            """_summary_
+    @height.setter
+    def height(self, value):
+        """_summary_
 
-            Args:
-                value (int): height of rectangle
-            """
-            if type(value) != int:
-                raise TypeError("height must be int")
-            if value <= 0:
-                raise ValueError("height must be > 0")
-            self.__height = value
+        Args:
+            value (int): height of rectangle
+        """
+        if type(value) != int:
+            raise TypeError("height must be int")
+        if value <= 0:
+            raise ValueError("height must be > 0")
+        self.__height = value
 
-        @property
-        def x(self):
-            """_summary_
-            """
-            return self.__x
+    @property
+    def x(self):
+        """_summary_
+        """
+        return self.__x
         
-        @x.setter
-        def x(self, value):
-            """_summary_
+    @x.setter
+    def x(self, value):
+        """_summary_
 
-            Args:
-                value (int): the horizontal position
-            """
-            if type(x) != int:
-                raise TypeError("x must be an int")
-            if x < 0:
-                raise ValueError("x must be >= 0")
-            self.x = value
+        Args:
+            value (int): the horizontal position
+        """
+        if type(x) != int:
+            raise TypeError("x must be an int")
+        if x < 0:
+            raise ValueError("x must be >= 0")
+        self.x = value
 
-        @property
-        def y(self):
-            """_summary_
-            """
-            return self.__y
+    @property
+    def y(self):
+        """_summary_
+        """
+        return self.__y
         
-        @y.setter
-        def y(self, value):
-            """_summary_
+    @y.setter
+    def y(self, value):
+        """_summary_
 
-            Args:
-                value (int): the vertical position
-            """
-            if type(y) != int:
-                raise TypeError("y must be an integer")
-            if y < 0:
-                raise ValueError("y must be >= 0")
-            self.__y = value
+        Args:
+            value (int): the vertical position
+        """
+        if type(y) != int:
+            raise TypeError("y must be an integer")
+        if y < 0:
+            raise ValueError("y must be >= 0")
+        self.__y = value
         
     def area(self):
         """_summary_
